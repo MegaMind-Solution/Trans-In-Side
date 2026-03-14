0cafccefd4d2b16f15987c7151c6a5a1f244a816
M Ramzan Ch <191318204+MegaMind-Solution@users.noreply.github.com>
2026-03-14 21:12:19 +0500

    ok

--- background.js
+++ background.js
@@ -13,9 +13,7 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-// Function to handle translation request from any source (context menu, shortcut)
 function requestTranslation(text, tab) {
-    // Store text as an object with a timestamp to guarantee the `onChanged` event fires.
     const newQuery = {
         text: text,
         ts: Date.now()
@@ -34,8 +32,6 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
 });
 
-// REMOVED: chrome.action.onClicked.addListener
-// (Removing this allows the default_popup in manifest.json to work normally!)
 
 chrome.commands.onCommand.addListener((command) => {
     if (command === "translate-shortcut") {
@@ -43,9 +39,7 @@ chrome.commands.onCommand.addListener((command) => {
             if (!tabs[0]) return;
             const tab = tabs[0];
 
-            // Open the panel immediately to respect the user gesture requirement.
             chrome.sidePanel.open({ windowId: tab.windowId }).then(() => {
-                // Then, get the selected text and update storage. The panel will react to the change.
                 chrome.scripting.executeScript({
                     target: { tabId: tab.id },
                     func: () => window.getSelection().toString()
@@ -55,15 +49,12 @@ chrome.commands.onCommand.addListener((command) => {
                         chrome.storage.local.set({ lastQuery: { text: text, ts: Date.now() } });
                     }
                 }).catch(err => {
-                    // This error is expected on chrome:// pages or other protected URLs.
-                    // We safely ignore it as the panel is already open and there is no text to get.
                 });
             });
         });
     }
 });
 
-// Listener for the quick tooltip translation API call
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'translateWord') {
         const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${request.lang}&dt=t&q=${encodeURIComponent(request.text)}`;
@@ -71,6 +62,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             .then(res => res.json())
             .then(data => sendResponse(data[0][0][0]))
             .catch(() => sendResponse("Translation error"));
-        return true; // Indicates an asynchronous response.
+        return true;
     }
 });
